92553841ab8845383a1446b23b4da6ea4dd64ab9
Vitaliy Yushkevich <vitaliy.yushkevich@team.bumble.com>
2024-03-14 22:19:56 +0400

    require PHPUnit, make simple test, testing Github Actions

---
+++ tests/GenDiffTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use function App\Finddiff\gendiff;
+
+class GenDiffTest extends TestCase
+{
+    public function testGendiff(): void
+    {
+        $expected ='{
+  + aaa: huq
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: 1
+  - zzz: true
+}';
+        $actual = gendiff(__DIR__ . '/fixtures/first.json',
+            '/Users/yushkevichv/PhpstormProjects/php-project-48/tests/fixtures/second.json');
+        $this->assertEquals($expected, $actual);
+    }
+}
