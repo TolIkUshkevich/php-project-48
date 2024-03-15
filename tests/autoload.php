c5b4ba7793464de04f0f76ec0ad7f16047bd9077
Vitaliy Yushkevich <vitaliy.yushkevich@team.bumble.com>
2024-03-15 14:09:38 +0400

    update workflows

--- tests/autoload.php
+++ tests/autoload.php
@@ -3,5 +3,5 @@
 if (file_exists(__DIR__ . '/../../../autoload.php')) {
     require_once __DIR__ . '/../../../autoload.php';
 } else {
-    require_once '/../vendor/autoload.php';
+    require_once __DIR__ . '/../vendor/autoload.php';
 }
