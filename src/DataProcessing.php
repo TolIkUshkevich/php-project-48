a98d3a77304715e41cb97eb91f9923e803f59563
TolIkUshkevich <104827125+TolIkUshkevich@users.noreply.github.com>
2024-05-15 16:09:22 +0300

    intermediate commit

--- src/DataProcessing.php
+++ src/DataProcessing.php
@@ -8,13 +8,30 @@ namespace App\DataProcessing;
  */
 function arrayBoolValuesSort(array $array): array
 {
-    foreach ($array as $key => $value) {
-        if (is_bool($value)) {
-            $newValue = $value ? 'true' : 'false';
-            $array[$key] = $newValue;
+    $resultArray = array_reduce(array_keys($array), function ($acc, $key) use ($array){
+        $value = $array[$key];
+        if (is_array($value)){
+            $acc[$key] = arrayBoolValuesSort($value);
         }
-    }
-    return $array;
+        if (is_bool($value) or $value === null) {
+            switch ($value):
+                case true:
+                    $newValue = 'true';
+                    break;
+                case false:
+                    $newValue = 'false';
+                    break;
+                case null:
+                    $newValue = 'null';
+                    break;
+                endswitch;
+        } else {
+            $newValue = $value;
+        }
+        $acc[$key] = $newValue;
+        return $acc;
+    });
+    return $resultArray;
 }
 
 function getValueStatus($key, $firstData, $secondData)
@@ -23,6 +40,8 @@ function getValueStatus($key, $firstData, $secondData)
         case [true, true]:
             if ($firstData[$key] === $secondData[$key]){
                 return 3;
+            } elseif (is_array($firstData[$key]) or is_array($secondData[$key])){
+                return 4;
             }
             return 2;
         case [true, false]:
@@ -36,6 +55,23 @@ function getValueByKey ($key, $firstData, $secondData = null){
     return $secondData === null ? $firstData[$key] : [$firstData[$key], $secondData[$key]];
 }
 
+function setParams(mixed $data, int $status, $deipth = 1){
+    if (!is_array($data)){
+        $result = ['value' => $data, 'status' => $status, 'deipth' => $deipth];
+        return $result;
+    }
+    $result = array_reduce(array_keys($data), function ($acc, $key) use ($data, $status, $deipth){
+        $value = $data[$key];
+        if (is_array($value)){
+            $acc[$key] = ['value' => setParams($value, $status, $deipth+1), 'status' => $status, 'deipth' => $deipth];
+        } else {
+            $acc[$key] = ['value' => $value, 'status' => $status, 'deipth' => $deipth];
+        }
+        return $acc;
+    });
+    return $result;
+}
+
 /**
  * @param array<mixed>$firstJsonData
  * @param array<mixed>$secondJsonData
@@ -46,34 +82,50 @@ function dataMerge(array $firstJsonData, array $secondJsonData, int $deipth = 1)
     $result = [];
     $firstJsonData = arrayBoolValuesSort($firstJsonData);
     $secondJsonData = arrayBoolValuesSort($secondJsonData);
-    $deipth = $deipth;
 
-    $result = array_reduce(array_merge(array_keys($firstJsonData), array_keys($secondJsonData)), function ($acc, $key) use ($firstJsonData, $secondJsonData, $deipth) {
+    $result = array_reduce(array_unique(array_merge(array_keys($firstJsonData), array_keys($secondJsonData))), function ($acc, $key) use ($firstJsonData, $secondJsonData, $deipth) {
         $status = getValueStatus($key, $firstJsonData, $secondJsonData);
         $value = 1;
         switch ($status):
+            case 4:
+                
             case 3:
                 $value = getValueByKey($key, $firstJsonData);
+                if (is_array($value)){
+                    $value = setParams($value, 3, $deipth++);
+                } else {
+                    $value = $value;
+                }
                 break;
             case 2:
                 $firstValue = getValueByKey($key, $firstJsonData);
                 $secondValue = getValueByKey($key, $secondJsonData);
                 if (is_array($firstValue) and is_array($secondValue)){
-                    $value = dataMerge($firstValue, $secondValue);
-                } else{
+                    $value = dataMerge($firstValue, $secondValue, $deipth+1);
+                } elseif (is_array($firstValue) or is_array($secondValue)){
+                    $value = [setParams($firstValue, 1), setParams($secondValue, 0)];
+                } else {
                     $value = [$firstValue, $secondValue];
                 }
                 break;
             case 1:
-                $value = getValueByKey($key, $firstJsonData);
+                if (is_array(getValueByKey($key, $firstJsonData))){
+                    $value = setParams(getValueByKey($key, $firstJsonData), 3, $deipth++);
+                } else {
+                    $value = getValueByKey($key, $firstJsonData);
+                }
                 break;
             case 0:
-                $value = getValueByKey($key, $secondJsonData);
+                if (is_array(getValueByKey($key, $secondJsonData))){
+                    $value = setParams(getValueByKey($key, $secondJsonData), 3, $deipth++);
+                } else {
+                    $value = getValueByKey($key, $secondJsonData);
+                }
                 break;
             endswitch;
         $acc[$key] = ['value' => $value, 'status' => $status, 'deipth' => $deipth];
         return $acc;
-    });
+    }); 
     ksort($result);
     return $result;
 }
@@ -84,34 +136,56 @@ function dataMerge(array $firstJsonData, array $secondJsonData, int $deipth = 1)
  * @param array<mixed>$secondJsonData
  * @return string $result
  */
-function formatingData(array $data, array $firstJsonData = null, array $secondJsonData = null): string
+function formatingData(array $data): string
 {
-    // var_dump($data);
     $result = "{\n";
     $result .= array_reduce(array_keys($data), function ($acc, $key) use ($data) {
-        var_dump($key);
         $value = $data[$key];
-        var_dump($value);
+        // print_r($value['deipth']);
+        // $deipth = $value['deipth'];
+        $deipth = 1;
+
+        // if ($value['status'] === null){
+        //     var_dump($key);
+        //     die;
+        // }
         switch ($value['status']):
             case 3:
-                $acc .= "  {$key}: {$value['value']}\n";
+                if (is_array($value['value'])){
+                    $acc .= formatingData($value['value']);
+                } else {
+                    $string = str_repeat("  ", $deipth);
+                    $acc .= "{$string}  {$key}: {$value['value']}\n";
+                }
                 break;
             case 2:
                 $firstValue = $value['value'][array_key_first($value['value'])];
                 $secondValue = $value['value'][array_key_last($value['value'])];
-                if (is_array($firstValue) and is_array($secondValue)){
+                if (is_array($firstValue) or is_array($secondValue)){
+                    $acc .= $key;
                     $acc .= formatingData($value['value']);
                     // var_dump($acc);
                 } else {
-                    $acc .= "- {$key}: {$firstValue}\n+ {$key}: {$secondValue}\n";
+                    $string = str_repeat("  ", $deipth);
+                    $acc .= "{$string}- {$key}: {$firstValue}\n{$string}+ {$key}: {$secondValue}\n";
                 }
                 break;
             case 1:
-                $acc .= "- {$key}: {$value['value']}\n";
+                $string = str_repeat("  ", $deipth);
+                if (is_array($value['value'])){
+                    $acc .= formatingData($value['value']);
+                } else{
+                    $acc .= "{$string}- {$key}: {$value['value']}\n";
+                }
                 break;
             case 0:
-                var_dump($key);
-                $acc .= "+ {$key}: {$value['value']}\n";
+                $currentValue = $value['value'];
+                if (is_array($currentValue)){
+                    $acc .= formatingData($value['value']);
+                } else { 
+                    $string = str_repeat("  ", $deipth);
+                    $acc .= "{$string}+ {$key}: {$currentValue}\n";
+                }
                 break;
         endswitch;
         return $acc;
--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -12,40 +12,74 @@ class GenDiffTest extends TestCase
     public function testGendiff(): void
     {
         $expected = '{
-  + aaa: huq
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-  - zzz: true
-}';
+            common: {
+              + follow: false
+                setting1: Value 1
+              - setting2: 200
+              - setting3: true
+              + setting3: null
+              + setting4: blah blah
+              + setting5: {
+                    key5: value5
+                }
+                setting6: {
+                    doge: {
+                      - wow:
+                      + wow: so much
+                    }
+                    key: value
+                  + ops: vops
+                }
+            }
+            group1: {
+              - baz: bas
+              + baz: bars
+                foo: bar
+              - nest: {
+                    key: value
+                }
+              + nest: str
+            }
+          - group2: {
+                abc: 12345
+                deep: {
+                    id: 45
+                }
+            }
+          + group3: {
+                deep: {
+                    id: {
+                        number: 45
+                    }
+                }
+                fee: 100500
+            }
+        }';
         $actual = gendiff(
             './tests/fixtures/first.json',
-            '/Users/tolya/projects/php-project-48/tests/fixtures/second.json'
+            '/Users/user/projects/php-project-48/tests/fixtures/second.json'
         );
         $this->assertEquals(
             $expected,
             $actual
         );
 
-        $expected = '{
-  + aaa: huq
-  - addition: asd
-  + host: hexlet.io
-  - name: nvidia
-  + timeout: 20
-  - tytle: 4080
-  + verbose: true
-}';
-        $actual = gendiff(
-            './tests/fixtures/first.yml',
-            './tests/fixtures/second.json'
-        );
-        $this->assertEquals(
-            $expected,
-            $actual
-        );
+//         $expected = '{
+//   + aaa: huq
+//   - addition: asd
+//   + host: hexlet.io
+//   - name: nvidia
+//   + timeout: 20
+//   - tytle: 4080
+//   + verbose: true
+// }';
+//         $actual = gendiff(
+//             './tests/fixtures/first.yml',
+//             './tests/fixtures/second.json'
+//         );
+//         $this->assertEquals(
+//             $expected,
+//             $actual
+//         );
     }
 }
