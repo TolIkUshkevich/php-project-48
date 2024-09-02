d4c4eda977057c92489a9439e2245ef67d039bca
Anatoliy Yushkevich <anatoliyyushkevich@MacBook-Pro-Anatoliy.local>
2024-09-02 21:52:11 +0300

    fix

--- parsers/parseFile.php
+++ parsers/parseFile.php
@@ -9,7 +9,8 @@ use function Parsers\YamlParser\parseYamlFile;
  * @param string $filePath
  * @return array<mixed>
  */
-function defineFormatAndParseFile (string $filePath){
+function defineFormatAndParseFile(string $filePath)
+{
     if (boolval(preg_match('/\w+\.json/', $filePath))) {
         return parseJsonFile($filePath);
     } elseif (boolval(preg_match('/\w+\.yml/', $filePath)) or boolval(preg_match('/\w+\.yaml/', $filePath))) {
